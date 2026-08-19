90e15020db33fd0145fa144a7c658c2e7bdf797c
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-19 17:31:20 +0100

    project init

---
+++ src/main/java/com/humana/humana_backend/HumanaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.humana.humana_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humana/humana_backend/HumanaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humana.humana_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
