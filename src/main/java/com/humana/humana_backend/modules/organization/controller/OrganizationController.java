359f532061037ba83c2438c97ca35415637e6bf8
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-20 12:19:40 +0100

    new organization endpoint

--- src/main/java/com/humana/humana_backend/modules/organization/controller/OrganizationController.java
+++ src/main/java/com/humana/humana_backend/modules/organization/controller/OrganizationController.java
@@ -1,2 +1,38 @@
-package com.humana.humana_backend.modules.organization.controller;public class OrganizationController {
+package com.humana.humana_backend.modules.organization.controller;
+
+import com.humana.humana_backend.modules.organization.dto.NewOrganizationDto;
+import com.humana.humana_backend.modules.organization.service.implementation.OrganizationAdderServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/organization")
+public class OrganizationController {
+
+    private final OrganizationAdderServiceImpl organizationAdderService;
+
+    public OrganizationController(OrganizationAdderServiceImpl organizationAdderService) {
+        this.organizationAdderService = organizationAdderService;
+    }
+
+    /**
+     * Creates a new organization.
+     *
+     * <p>The request body is validated before being passed to the service layer.</p>
+     *
+     * @param dto the data required to create the organization
+     * @return a response containing the newly created organization
+     */
+    @PostMapping("/new")
+    public ResponseEntity<?> addNewOrganization(
+            @Valid @RequestBody NewOrganizationDto dto
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(organizationAdderService.add(dto));
+    }
 }
