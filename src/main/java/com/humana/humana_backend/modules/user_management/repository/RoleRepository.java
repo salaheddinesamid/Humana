997537fa6815cd250921212830fcc9f05c50617c
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-20 12:55:56 +0100

    update

--- src/main/java/com/humana/humana_backend/modules/user_management/repository/RoleRepository.java
+++ src/main/java/com/humana/humana_backend/modules/user_management/repository/RoleRepository.java
@@ -1,7 +1,11 @@
 package com.humana.humana_backend.modules.user_management.repository;
 
 import com.humana.humana_backend.modules.user_management.model.Role;
+import com.humana.humana_backend.modules.user_management.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(RoleName roleName);
 }
