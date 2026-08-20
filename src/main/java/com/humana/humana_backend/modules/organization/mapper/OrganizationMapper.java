f9af2919611f2cae6eff7323fd4a593ad0e1ce72
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-20 12:19:56 +0100

    add organization dto mapper

--- src/main/java/com/humana/humana_backend/modules/organization/mapper/OrganizationMapper.java
+++ src/main/java/com/humana/humana_backend/modules/organization/mapper/OrganizationMapper.java
@@ -1,7 +1,22 @@
 package com.humana.humana_backend.modules.organization.mapper;
 
+import com.humana.humana_backend.modules.organization.dto.NewOrganizationDto;
+import com.humana.humana_backend.modules.organization.model.Organization;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrganizationMapper {
+
+    public Organization mapDto(
+            NewOrganizationDto dto
+    ){
+        return new Organization(
+                dto.getLegalName(),
+                dto.getRC(),
+                dto.getEmail(),
+                dto.getPhone(),
+                dto.getCountry(),
+                dto.getNumberOfEmployees()
+        );
+    }
 }
