4eb998e567009458746601a21a2f147ce79bc109
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-20 11:30:42 +0100

    init testing

---
+++ src/test/java/com/humana/humana_backend/auth/integration/AuthenticationIntegrationTest.java
@@ -0,0 +1,7 @@
+package com.humana.humana_backend.auth.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthenticationIntegrationTest {
+}
---
+++ src/test/java/com/humana/humana_backend/auth/unit/AuthenticationServiceUnitTest.java
@@ -0,0 +1,12 @@
+package com.humana.humana_backend.auth.unit;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthenticationServiceUnitTest {
+
+    @Test
+    void testAuthenticationSuccess(){}
+
+    @Test
+    void testAuthenticationUserNotFound(){}
+}
