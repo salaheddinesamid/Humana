750a26705cc5ba3d2543ab4510d53d519da25450
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-20 18:21:44 +0100

    patch endpoint for organization update

--- src/main/java/com/humana/humana_backend/modules/organization/controller/OrganizationController.java
+++ src/main/java/com/humana/humana_backend/modules/organization/controller/OrganizationController.java
@@ -1,9 +1,12 @@
 package com.humana.humana_backend.modules.organization.controller;
 
 import com.humana.humana_backend.modules.organization.dto.NewOrganizationDto;
+import com.humana.humana_backend.modules.organization.dto.OrganizationUpdateDto;
 import com.humana.humana_backend.modules.organization.service.implementation.OrganizationAdderServiceImpl;
 import com.humana.humana_backend.modules.organization.service.implementation.OrganizationQueryServiceImpl;
+import com.humana.humana_backend.modules.organization.service.implementation.OrganizationUpdateServiceImpl;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +19,12 @@ public class OrganizationController {
 
     private final OrganizationAdderServiceImpl organizationAdderService;
     private final OrganizationQueryServiceImpl organizationQueryService;
+    private final OrganizationUpdateServiceImpl organizationUpdateService;
 
-    public OrganizationController(OrganizationAdderServiceImpl organizationAdderService, OrganizationQueryServiceImpl organizationQueryService) {
+    public OrganizationController(OrganizationAdderServiceImpl organizationAdderService, OrganizationQueryServiceImpl organizationQueryService, OrganizationUpdateServiceImpl organizationUpdateService) {
         this.organizationAdderService = organizationAdderService;
         this.organizationQueryService = organizationQueryService;
+        this.organizationUpdateService = organizationUpdateService;
     }
 
     /**
@@ -48,5 +53,21 @@ public class OrganizationController {
     public ResponseEntity<?> getOrganization(@PathVariable UUID id){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(organizationQueryService.getOrganization(id));
+    }
+
+    /**
+     * Updates an existing organization.
+     *
+     * @param id the unique identifier of the organization to update
+     * @param dto the data containing the fields to be updated
+     * @return a response containing the updated organization
+     */
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<?> updateOrganization(@PathVariable UUID id, @RequestBody OrganizationUpdateDto dto){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(organizationUpdateService.update(
+                        id, dto
+                ));
     }
 }
