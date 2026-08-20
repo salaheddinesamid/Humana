0eef3a030eb9d94c4429d81df7c47ea4c0955156
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-20 13:07:00 +0100

    update

--- src/main/java/com/humana/humana_backend/modules/organization/service/OrganizationQueryService.java
+++ src/main/java/com/humana/humana_backend/modules/organization/service/OrganizationQueryService.java
@@ -1,2 +1,10 @@
-package com.humana.humana_backend.modules.organization.service;public interface OrganizationQueryService {
+package com.humana.humana_backend.modules.organization.service;
+
+import com.humana.humana_backend.modules.organization.model.Organization;
+
+import java.util.UUID;
+
+public interface OrganizationQueryService {
+
+    Organization getOrganization(UUID id);
 }
--- src/main/java/com/humana/humana_backend/modules/organization/service/implementation/OrganizationQueryServiceImpl.java
+++ src/main/java/com/humana/humana_backend/modules/organization/service/implementation/OrganizationQueryServiceImpl.java
@@ -1,2 +1,25 @@
-package com.humana.humana_backend.modules.organization.service.implementation;public class OrganizationQueryServiceImpl {
+package com.humana.humana_backend.modules.organization.service.implementation;
+
+import com.humana.humana_backend.modules.organization.model.Organization;
+import com.humana.humana_backend.modules.organization.repository.OrganizationRepository;
+import com.humana.humana_backend.modules.organization.service.OrganizationQueryService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrganizationQueryServiceImpl implements OrganizationQueryService {
+
+    private final OrganizationRepository organizationRepository;
+
+    public OrganizationQueryServiceImpl(OrganizationRepository organizationRepository) {
+        this.organizationRepository = organizationRepository;
+    }
+
+    @Override
+    public Organization getOrganization(UUID id) {
+        return organizationRepository
+                .findById(id)
+                .orElseThrow();
+    }
 }
