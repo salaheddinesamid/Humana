22c2e272c2da09e8fc85a5013f735c0f17bb2781
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-20 13:06:52 +0100

    get organization details endpoint

--- src/main/java/com/humana/humana_backend/modules/organization/controller/OrganizationController.java
+++ src/main/java/com/humana/humana_backend/modules/organization/controller/OrganizationController.java
@@ -2,22 +2,24 @@ package com.humana.humana_backend.modules.organization.controller;
 
 import com.humana.humana_backend.modules.organization.dto.NewOrganizationDto;
 import com.humana.humana_backend.modules.organization.service.implementation.OrganizationAdderServiceImpl;
+import com.humana.humana_backend.modules.organization.service.implementation.OrganizationQueryServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/organization")
 public class OrganizationController {
 
     private final OrganizationAdderServiceImpl organizationAdderService;
+    private final OrganizationQueryServiceImpl organizationQueryService;
 
-    public OrganizationController(OrganizationAdderServiceImpl organizationAdderService) {
+    public OrganizationController(OrganizationAdderServiceImpl organizationAdderService, OrganizationQueryServiceImpl organizationQueryService) {
         this.organizationAdderService = organizationAdderService;
+        this.organizationQueryService = organizationQueryService;
     }
 
     /**
@@ -34,5 +36,17 @@ public class OrganizationController {
     ) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(organizationAdderService.add(dto));
+    }
+
+    /**
+     * Retrieves an organization by its unique identifier.
+     *
+     * @param id the unique identifier of the organization
+     * @return a response containing the requested organization
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<?> getOrganization(@PathVariable UUID id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(organizationQueryService.getOrganization(id));
     }
 }
