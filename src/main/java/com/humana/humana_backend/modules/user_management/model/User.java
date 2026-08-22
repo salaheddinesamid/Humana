122bda861bdfefb686f13b125590453f5aa089c1
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-22 11:32:30 +0100

    extract user authorities

--- src/main/java/com/humana/humana_backend/modules/user_management/model/User.java
+++ src/main/java/com/humana/humana_backend/modules/user_management/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -35,7 +36,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return
+                this.roles.stream().map(role -> new SimpleGrantedAuthority(role.getRoleName().toString()))
+                        .toList();
     }
 
     @Override
