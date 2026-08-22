b1c9a91cc5e55bbc7587c934e70204240437b274
salaheddinesamid <salaheddine.kobra@iu-study.org>
2026-08-22 11:32:53 +0100

    implement RBAC filter

--- src/main/java/com/humana/humana_backend/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/humana/humana_backend/security/filter/JwtAuthenticationFilter.java
@@ -1,2 +1,53 @@
-package com.humana.humana_backend.security.filter;public class JwtAuthenticationFilter {
+package com.humana.humana_backend.security.filter;
+
+import com.humana.humana_backend.config.UserDetailsServiceImpl;
+import com.humana.humana_backend.modules.user_management.model.User;
+import com.humana.humana_backend.security.jwt.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtAuthenticationFilter(JwtUtils jwtUtils, UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Extract JWT token
+        String token = jwtUtils.getToken(request);
+
+        if (token != null && jwtUtils.validateToken(token)){
+            // Extract the username:
+            String username = jwtUtils.extractUserName(token);
+
+            // Fetch the user:
+            UserDetails userDetails = (User) userDetailsService.loadUserByUsername(username);
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(
+                            username, null, userDetails.getAuthorities()
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
